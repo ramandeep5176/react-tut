da87ef910bc8ac56dba32c5cd0675ac40fa68b8e
ramandeep5176 <ramandeepgill5176@gmail.com>
2021-09-19 12:16:43 -0400

    1st-component

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 import React from "react";
+import ReactDom from "react-dom";
+
+// function Greeting() {
+//   return <h1>i'm raamn</h1>;
+// }
+const  Greeting=()=> {
+  return React.createElement('h1',{},'raman')
+}
+
+ReactDom.render(<Greeting />, document.getElementById("root"));
