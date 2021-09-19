aa5a2d63e2b9090e7d98bc6bb69a203488de695f
ramandeep5176 <ramandeepgill5176@gmail.com>
2021-09-19 18:13:13 -0400

    Props

--- src/index.js
+++ src/index.js
@@ -1,11 +1,46 @@
 import React from "react";
 import ReactDom from "react-dom";
 
-// function Greeting() {
-//   return <h1>i'm raamn</h1>;
-// }
-const  Greeting=()=> {
-  return React.createElement('h1',{},'raman')
+// css
+import "./index.css";
+// setup vars
+const firstBook = {
+  img: "https://images-na.ssl-images-amazon.com/images/I/51xPyfcG1IL._SX329_BO1,204,203,200_.jpg",
+  title: "You Are a Badass at Making Money: Master the Mindset of Wealth",
+  author: "Jen Sincero",
+};
+const secondBook = {
+  img: "https://images-na.ssl-images-amazon.com/images/I/41ApNA6CwTL._SX331_BO1,204,203,200_.jpg",
+  title: `can't hurt me`,
+  author: `david goggins`,
+};
+
+function BookList() {
+  return (
+    <section className="booklist">
+      <Book
+        img={firstBook.img}
+        title={firstBook.title}
+        author={firstBook.author}
+      />
+      <Book
+        img={secondBook.img}
+        title={secondBook.title}
+        author={secondBook.author}
+      />
+    </section>
+  );
 }
 
-ReactDom.render(<Greeting />, document.getElementById("root"));
+const Book = ({ img, title, author }) => {
+  return (
+    <article>
+      <img src={img} />
+      <h1>{title}</h1>
+      <h4>{author}</h4>
+    </article>
+  );
+};
+
+//
+ReactDom.render(<BookList />, document.getElementById("root"));
